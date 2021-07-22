890ffdb9afda16ea6bf9411593f70e8331a357ca
reda777 <redazara1998@gmail.com>
2021-07-22 14:28:50 +0100

    added random colors and website looks better now

--- scripte.js
+++ scripte.js
@@ -1,36 +1,49 @@
-const container=document.querySelector('.container');
-createDiv();
-function createDiv(dim=16){
+createGrid();
+function createGrid(dim=16){
+    const container=document.querySelector('.container');
     for(let i=0;i<dim*dim;i++){
         const content=document.createElement('div');
-        content.classList.add('content');
+        content.style.backgroundColor='rgb(255,255,255)';
         container.appendChild(content);
     }
+    const divs=document.querySelectorAll('.container div');
+    divs.forEach(div => {
+        div.addEventListener('mouseover',(e) =>{
+            div.style.backgroundColor='rgb(0,0,0)';
+        });
+    });
+    if(dim!==16){
+        divs.forEach(div => {
+            div.classList.remove('divHover');
+        });
+        container.style['grid-template-columns']='repeat('+dim+','+480/dim+'px)';
+        container.style['grid-template-rows']='repeat('+dim+','+480/dim+'px)';
+    }
 }
-
-const divs=document.querySelectorAll('.content');
-divs.forEach(div => {
-    div.addEventListener('mouseover',changeColor);
+//change grid size
+const refresh=document.querySelector('.refresh');
+refresh.addEventListener('click',()=>{
+    let userInput=prompt('enter dimension of the grid');
+    createGrid(userInput);
+});
+//random colors
+const random=document.querySelector('.random');
+random.addEventListener('click',()=>{
+    const divs=document.querySelectorAll('.container div');
+    divs.forEach(div => {
+        div.addEventListener('mouseover',(e) =>{
+            div.style.backgroundColor='rgb('+getRandomInt()+','+getRandomInt()+','+getRandomInt()+')';
+        });
+    });
+});
+function getRandomInt(max=256) {
+    return Math.floor(Math.random() * max);
+}
+//shades of black
+const shades=document.querySelector('.shades');
+shades.addEventListener('click',()=>{
+    let userInput=prompt('enter dimension of the grid');
+    createGrid(userInput);
 });
 
-function changeColor(e){
-    this.classList.add('divHover');
-}
-
-const button=document.querySelector('.refresh');
-button.addEventListener('click',removeColor);
-function removeColor(e){
-    const divs1=document.querySelectorAll('.content');
-    divs1.forEach(div => {
-        div.classList.remove('divHover');
-    });
-    let userInput=prompt('enter number of squares');
-    createDiv(userInput);
-    container.style['grid-template-columns']='repeat('+userInput+','+480/userInput+'px)';
-    container.style['grid-template-rows']='repeat('+userInput+','+480/userInput+'px)';
-    const divs2=document.querySelectorAll('.content');
-    divs2.forEach(div => {
-        div.addEventListener('mouseover',changeColor);
-    });
-}
 
