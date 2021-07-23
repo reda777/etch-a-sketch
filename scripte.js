f39f32411d314b509d46fc4f84643ca09d7f1917
reda777 <redazara1998@gmail.com>
2021-07-23 14:45:09 +0100

    finished layout still need to fix divs stacking up

--- scripte.js
+++ scripte.js
@@ -1,18 +1,40 @@
 createGrid();
+//change grid size
+const dimension=document.querySelector('.dimension');
+dimension.addEventListener('click',()=>{
+    let userInput=prompt('enter dimension of the grid');
+    createGrid(userInput);
+});
+//rainbow colors
+const rainbow=document.querySelector('.rainbow');
+rainbow.addEventListener('click',()=>{
+    resetColor();
+    hoverColor();
+});
+//black gradient
+const gradient=document.querySelector('.gradient');
+gradient.addEventListener('click',()=>{
+    let grad=255;
+    resetColor();
+    const divs=document.querySelectorAll('.container div');
+    divs.forEach(div => {
+        div.addEventListener('mouseover',() =>{
+            div.style.backgroundColor='rgb('+grad--+','+grad--+','+grad--+')';
+        });
+    });
+});
+
 function createGrid(dim=16){
     const container=document.querySelector('.container');
     for(let i=0;i<dim*dim;i++){
         const content=document.createElement('div');
-        content.style.backgroundColor='rgb(255,255,255)';
+        content.classList.add('content');
         container.appendChild(content);
     }
-    const divs=document.querySelectorAll('.container div');
-    divs.forEach(div => {
-        div.addEventListener('mouseover',(e) =>{
-            div.style.backgroundColor='rgb(0,0,0)';
-        });
-    });
+    resetColor();
+    hoverColor(0);
     if(dim!==16){
+        const divs=document.querySelectorAll('.container div');
         divs.forEach(div => {
             div.classList.remove('divHover');
         });
@@ -20,30 +42,29 @@ function createGrid(dim=16){
         container.style['grid-template-rows']='repeat('+dim+','+480/dim+'px)';
     }
 }
-//change grid size
-const refresh=document.querySelector('.refresh');
-refresh.addEventListener('click',()=>{
-    let userInput=prompt('enter dimension of the grid');
-    createGrid(userInput);
-});
-//random colors
-const random=document.querySelector('.random');
-random.addEventListener('click',()=>{
+
+function hoverColor(color){
     const divs=document.querySelectorAll('.container div');
     divs.forEach(div => {
-        div.addEventListener('mouseover',(e) =>{
-            div.style.backgroundColor='rgb('+getRandomInt()+','+getRandomInt()+','+getRandomInt()+')';
+        div.addEventListener('mouseover',() =>{
+            if(typeof color === 'number')
+                div.style.backgroundColor='rgb('+color+','+color+','+color+')';
+            else
+                div.style.backgroundColor='rgb('+getRandomInt()+','+getRandomInt()+','+getRandomInt()+')';
         });
     });
-});
+}
+
+function resetColor(){
+    const divs=document.querySelectorAll('.container div');
+    divs.forEach(div => {
+        div.style.backgroundColor='rgb(255,255,255)';
+    });
+}
+
 function getRandomInt(max=256) {
     return Math.floor(Math.random() * max);
 }
-//shades of black
-const shades=document.querySelector('.shades');
-shades.addEventListener('click',()=>{
-    let userInput=prompt('enter dimension of the grid');
-    createGrid(userInput);
-});
+
 
 
